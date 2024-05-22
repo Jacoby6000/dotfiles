b57ea4ad13ee153cd9003bd074ad9bc33a04cf3e
Jacob Barber <jacob.barber@disney.com>
2024-05-22 11:34:46 -0500

    Fix dap descriptions

--- neovim/lua/lib/lazy_mapping.lua
+++ neovim/lua/lib/lazy_mapping.lua
@@ -3,7 +3,12 @@ local M = {}
 function M.map(keys, description, func, opts)
   opts = opts or {}
   opts.desc = description
-  return { keys, func, opts }
+  return {
+    keys,
+    func,
+    desc = description,
+    opts,
+  }
 end
 
 return M
--- neovim/lua/plugins/nvim-dap.lua
+++ neovim/lua/plugins/nvim-dap.lua
@@ -5,30 +5,30 @@ return {
   keys = function()
     local dap = require("dap")
     return {
-      LazyMapping.map("<leader>dc", "Continue Execution", function()
+      LazyMapping.map("<leader>dc", "Continue DAP Execution", function()
         dap.continue()
       end),
 
-      LazyMapping.map("<leader>dr", function()
+      LazyMapping.map("<leader>dr", "Toggle DAP Repl", function()
         dap.repl.toggle()
       end),
 
-      LazyMapping.map("<leader>dK", function()
+      LazyMapping.map("<leader>dK", "Show DAP hover info", function()
         require("dap.ui.widgets").hover()
       end),
-      LazyMapping.map("<leader>dt", function()
+      LazyMapping.map("<leader>dt", "Toggle DAP Breakpoint", function()
         dap.toggle_breakpoint()
       end),
 
-      LazyMapping.map("<leader>dso", function()
+      LazyMapping.map("<leader>dso", "Step over function in DAP", function()
         dap.step_over()
       end),
 
-      LazyMapping.map("<leader>dsi", function()
+      LazyMapping.map("<leader>dsi", "Step in to function in DAP", function()
         dap.step_into()
       end),
 
-      LazyMapping.map("<leader>dl", function()
+      LazyMapping.map("<leader>dl", "Rerun last execution with DAP", function()
         dap.run_last()
       end),
     }
